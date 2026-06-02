ef881801dc72e2f95ec3ce6cdb102e4b6cb42b62
THIAGO TAVARES <95090226+tavaresmirako@users.noreply.github.com>
2026-06-02 10:53:13 -0300

    feat: add structured logger utility

---
+++ src/utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+
+def setup_logger(name: str = "xml-auditoria") -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
